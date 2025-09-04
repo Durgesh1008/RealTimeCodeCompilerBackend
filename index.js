4140cfc7a7f7f8e7707b7f17645e47ac602af280
Durgeshchaudhary001 <dk1570801@gmail.com>
2025-09-04 16:04:03 +0530

    fix 206

--- index.js
+++ index.js
@@ -206,9 +206,10 @@ const __dirname = path.resolve();
 
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-app.get("*", (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
+
 
 
 server.listen(port, () => {
